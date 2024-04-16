e575ac3f1c5ab7f20106616cccc873673bebe414
tester7894 <167072562+tester7894@users.noreply.github.com>
2024-04-16 14:22:57 +0400

    Update clearing_exceptions.py

--- clearing_exceptions.py
+++ clearing_exceptions.py
@@ -1,14 +1,30 @@
 import csv
+import re
+
+with open('exceptions.txt', 'r') as file_txt:
+    txt_domains = {row.strip() for row in file_txt}
 
 with open('test.csv', 'r') as file_csv:
-    csv_data = {row['Domain'] for row in csv.DictReader(file_csv)}
+    reader = csv.DictReader(file_csv)
+    
+    # Проверяем наличие столбца 'Email' в заголовке CSV файла
+    if 'Domain' not in reader.fieldnames:
+        print("Столбец 'Domain' не найден в CSV файле.")
+        exit(1)
 
-with open('list_uns.txt', 'r') as file:
-    for list_domain in file.readlines():
-        lid = list_domain.strip()  
+    for row in reader:
+        email = row['Domain']
 
-        if lid in csv_data:
-            print("Нашел совпадение:", lid)
-            with open('result.txt', 'a+') as result:
-                res = result.write(f'{lid}\n')
+        if email in txt_domains:
+            print("Найдено точное совпадение:", email)
+            with open('result.txt', 'a+') as result_file:
+                result_file.write(f'{email}\n')
+        else:
+            match = re.search(r'@(.+)', email)
+            if match:
+                domain = match.group(1)
 
+                if domain in txt_domains:
+                    print("Найдено совпадение по домену:", email)
+                    with open('result.txt', 'a+') as result_file:
+                        result_file.write(f'{email}\n')
